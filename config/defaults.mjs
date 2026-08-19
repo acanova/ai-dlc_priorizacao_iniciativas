5bd19424515104421dace37a06f2a7c6696194ff
Alexandre F. Canova <alexandre.canova@gmail.com>
2026-08-19 20:15:34 -0300

    starter

--- config/defaults.mjs
+++
@@ -1,6 +0,0 @@
-import { resolve } from "node:path";
-
-export const DEFAULT_HOST = "127.0.0.1";
-export const DEFAULT_PORT = 3000;
-export const DEFAULT_DATA_FILE = resolve("data/initiatives.json");
-export const DEFAULT_BODY_LIMIT_BYTES = 64 * 1024;
--- src/errors/application-error.mjs
+++
@@ -1,8 +0,0 @@
-export class ApplicationError extends Error {
-  constructor(code, message, details, options = {}) {
-    super(message, options);
-    this.name = "ApplicationError";
-    this.code = code;
-    if (details !== undefined) this.details = details;
-  }
-}
